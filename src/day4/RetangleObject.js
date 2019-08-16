64c1f2568e812d11065688f1441bb7302cb08d7c
Eduardo Formiga <eduardo.formiga@gmail.com>
2019-08-15 23:31:13 -0300

    chore: added Day 4 challenges

---
+++ src/day4/RetangleObject.js
@@ -0,0 +1,11 @@
+/*
+ * Complete the Rectangle function
+ */
+function Rectangle(a, b) {
+    return {
+        length: a,
+        width: b,
+        perimeter: 2 * (a + b),
+        area: a*b
+    }
+}
